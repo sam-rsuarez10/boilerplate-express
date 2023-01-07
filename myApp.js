62981f932b3b3a5e5514738908979e1226acba5b
sam-rsuarez10 <suarezzcode2001@gmail.com>
2023-01-07 14:25:37 -0300

    Complete .env challenge

--- myApp.js
+++ myApp.js
@@ -1,39 +1,18 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
-console.log("Hello world");
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.use('/public', express.static(__dirname + '/public'));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/json', (req, res) => {
+    let obj = { "message": "Hello json" };
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        obj.message = obj.message.toUpperCase();
+    }
+    return res.json(obj);
+});
 
 
  module.exports = app;
